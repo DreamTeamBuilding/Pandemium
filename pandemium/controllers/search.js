fbd5429953388aaaaa0218a0c1f06c85feca8824
Romain <rom.gouttefangeas@gmail.com>
2017-12-05 14:42:31 +0100

    Verification de l'existence du dossier cache

--- pandemium/controllers/search.js
+++ pandemium/controllers/search.js
@@ -4,8 +4,13 @@ var fs = require('fs');
 
 exports.search = function(req, res) {
 	// Check if in cache
+	var cacheDir = "./cache";
+	if(!fs.existsSync(cacheDir))
+  {
+		fs.mkdirSync(cacheDir);
+	}
 	var requete = req.params.query;
-	var dir = './cache/'+requete;
+	var dir = cacheDir+'/'+requete;
   if(!fs.existsSync(dir))
   {
 		//Not in cache, do request
@@ -16,4 +21,5 @@ exports.search = function(req, res) {
 		});
   }
 	// Read the cache
+	res.end();
 }
