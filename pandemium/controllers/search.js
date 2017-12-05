eab9fb8b6fb71709a36e372a5fb3eae4e204832c
Romain <rom.gouttefangeas@gmail.com>
2017-12-05 11:04:24 +0100

    Refactoring

--- pandemium/controllers/search.js
+++ pandemium/controllers/search.js
@@ -6,19 +6,14 @@ exports.search = function(req, res) {
 	// Check if in cache
 	var requete = req.params.query;
 	var dir = './cache/'+requete;
-  if(fs.existsSync(dir))
+  if(!fs.existsSync(dir))
   {
-		//Petit affichage visuel, en cours de modification
-		res.setHeader('content-type', 'text/html');
-		res.write("Requete deja dans le cache");
-		res.end();
-  }
-	else {
 		//Not in cache, do request
 		search.search(requete, function(queryResult) {
 				extract.extractContent(requete, queryResult);
 				res.setHeader('content-type', 'application/json');
 				res.send(queryResult);
 		});
-	}
+  }
+	// Read the cache
 }
