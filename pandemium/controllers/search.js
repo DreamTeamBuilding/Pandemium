728afa8087775be5f3d73f0301113cc74015ac60
Victor <victorduvercors@gmail.com>
2017-12-05 10:53:47 +0100

    Victor : Verif que la requete est pas dans le cache

--- pandemium/controllers/search.js
+++ pandemium/controllers/search.js
@@ -1,11 +1,21 @@
 var search = require('../models/search/search');
 var extract = require('../models/extract/extract');
+var fs = require('fs');
 
 exports.search = function(req, res) {
 	// Check if in cache
-	search.search(req.params.query, function(queryResult) {
-    	res.setHeader('content-type', 'application/json');
-			extract.extractContent(req.params.query, queryResult);
-			res.send(queryResult);
-	});
+	var requete = req.params.query;
+	var dir = './cache/'+requete;
+  if(fs.existsSync(dir))
+  {
+		console.log("Cette recherche est dans le cache")
+		// extract.
+  }
+	else {
+		search.search(requete, function(queryResult) {
+	    	res.setHeader('content-type', 'application/json');
+				extract.extractContent(requete, queryResult);
+				res.send(queryResult);
+		});
+	}
 }
