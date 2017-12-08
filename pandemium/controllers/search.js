632527b36a6e256456eaa5355319847f057b6f00
Romain <rom.gouttefangeas@gmail.com>
2017-12-08 09:57:07 +0100

    Recherche Sparql en dur

--- pandemium/controllers/search.js
+++ pandemium/controllers/search.js
@@ -1,5 +1,6 @@
 var search = require('../models/search/search');
 var spotlight = require('../models/spotlight/testSpotlight');
+var sparql = require('sparql');
 
 exports.search = function(req, res) {
 	search.search(escape(req.params.query), function(queryResult) {
@@ -12,5 +13,14 @@ exports.spotlightSearch = function(req, res) {
 	spotlight.search(function(queryResult) {
     	res.setHeader('content-type', 'application/json');
     	res.send(queryResult);
+	});
+}
+
+exports.sparqlSearch = function(req, res) {
+	var request = 'PREFIX res: <http://dbpedia.org/resource/> SELECT res:Cardiomyopathy ?p ?n WHERE {res:Cardiomyopathy ?p ?n.}';
+	var client = new sparql.Client('http://dbpedia.org/sparql');
+	client.query(request, function(err, result) {
+		res.setHeader('content-type', 'application/json');
+		res.send(result);
 	});
 }
--- pandemium/routes/search.js
+++ pandemium/routes/search.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
   res.redirect('/');
 });
 router.get('/testSpotlight', searchController.spotlightSearch);
+router.get('/sparqlSearch', searchController.sparqlSearch);
 router.get('/:query', searchController.search);
 
 module.exports = router;
