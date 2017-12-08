fb725c7f4bb7b4bdd2c566b779bb8a5ccd516f28
Piwit <lucas.marie.2b@gmail.com>
2017-12-08 15:09:08 +0100

    Ajout async au lieu de boucle asynchrone mais synchrone affreuse

--- pandemium/models/extract/extract.js
+++ pandemium/models/extract/extract.js
@@ -1,49 +1,28 @@
 var textract = require('textract');
 var fs = require('fs');
+var async = require('async');
 
 exports.extractContent = function(query, jsonAsString, callback) {
   var queryResultJsonObj = JSON.parse(jsonAsString);
   var dir = './cache/'+query;
-  if(!fs.existsSync(dir))
-  {
+  if(!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
-
-  var uneIteration = function(loop, i){
-    var url = queryResultJsonObj.items[i].link;
-    extractUrl(url, dir, i, function() {
-      // Ici on est dans le callback de la fonction asynchrone
-      loop();
-    })
-  };
-
-  asyncLoop({
-    length : queryResultJsonObj.items.length,
-    functionToLoop : uneIteration,
-    callback : function(){
-      console.log("CALLBACK CALLED");
-      callback(query);
-    }
+  extractUrl.dir = dir;
+  async.eachOf(queryResultJsonObj.items, extractUrl, function(err) {
+    if(err)
+      console.log("ERROR " + err);
+    callback(query);
   });
+
 }
 
-function extractUrl(url, dir, index, callback) {
+function extractUrl(item, index, callback) {
+  var url = item.link;
   textract.fromUrl(url, function(error, text) {
-    fs.appendFileSync(dir+'/content'+index, text);
-    callback();
+    fs.appendFile(extractUrl.dir+'/content'+index, text, function() {
+      callback();
+      console.log(url + " downloaded");    
+    });
   });
-  console.log(url + "DONE");
-}
-
-// TODO optimiser la loop
-// Permet de rendre une boucle asynchrone synchrone
-function asyncLoop(o){
-    var i=-1;
-
-    var loop = function(){
-        i++;
-        if(i==o.length){o.callback(); return;}
-        o.functionToLoop(loop, i);
-    }
-    loop();
 }
