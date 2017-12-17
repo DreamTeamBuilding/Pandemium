8b3e2f77157b4ceb7df4a83bd3b24f6c30aa762b
Lucas OH <luky.herbin@free.fr>
2017-12-17 12:11:46 +0100

    [LOH] Le slider déclanche un event

--- pandemium/public/js/slider.js
+++ pandemium/public/js/slider.js
@@ -1,7 +1,13 @@
 // With JQuery
 $('#toleranceSlider').bind("input",setTolerance);
+$('#toleranceSlider').bind("change",calculateWithTolerance);
 
 function setTolerance(){
   var value = document.getElementById('toleranceSlider').value;
   $('#seuilToleranceTxt').html(" " + value);
+}
+
+function calculateWithTolerance(){
+  var value = document.getElementById('toleranceSlider').value;
+  alert('On va relancer le calcul avec une tolérance de  : ' + value);
 }
