9ca851253cf84b46bc0c89dce177f1f9e422bb33
Victor Bonin <victorduvercors@gmail.com>
2017-12-18 02:12:27 +0100

    Victor : nom des resources

--- pandemium/models/extract/extract.js
+++ pandemium/models/extract/extract.js
@@ -1,9 +1,11 @@
 var textract = require('textract');
 var fs = require('fs');
 var async = require('async');
+var requete;
 
 exports.extractContent = function(query, queryResult, callback) {
 	var dir = './cache/'+query;
+	requete = query;
 	if(!fs.existsSync(dir)) {
 		fs.mkdirSync(dir);
 	}
@@ -18,10 +20,12 @@ exports.extractContent = function(query, queryResult, callback) {
 
 function extractUrl(item, index, callback) {
 	var url = item.link;
+	var name = requete + " - "+ url;
 	textract.fromUrl(url, function(error, text) {
 		fs.appendFile(extractUrl.dir+'/content'+index, text, function() {
 			callback();
-			console.log(url + " downloaded");    
+			console.log(url + " downloaded");
+			console.log("Name : "+name);
 		});
 	});
 }
